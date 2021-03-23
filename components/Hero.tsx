affbb4752c02ab38db38cef21c89424355445949
Dan Choiniere <dchoinie@gmail.com>
2021-03-22 22:48:25 -0500

    Hero redesign, next images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image'
 
 const Hero = () => {
     return (
-        <div></div>
+        <div className="hero">
+            <div className="flex justify-center items-center w-full h-full" style={{ marginTop: "-123.2px"}}>
+                <h1 className="font-bold text-6xl text-white" style={{ textShadow: "1px 1px 15px black"}}>
+                    Trinity Evangelical Lutheran Church
+                </h1>
+            </div>
+        </div>
     )
 }
 
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Button from '../../components/Button'
-import { FaBars, FaPrayingHands } from 'react-icons/fa'
+import { FaBars, FaEnvelope, FaFacebook, FaPrayingHands, FaYoutube } from 'react-icons/fa'
 import { GrClose } from 'react-icons/gr'
 
 
@@ -10,58 +10,71 @@ const Header = (): JSX.Element => {
     return (
         <div className="max-w-screen-xl block lg:flex lg:flex-row mx-auto py-2">
             <div className="flex w-full justify-between lg:w-1/4 px-4 lg:pl-0 mb-2 lg:mb-0">
-                <Image src="/images/LCMS_logo.png" width={75} height={100} />
+                <ul className="flex items-center text-red-900">
+                    <li className="mr-2">
+                        <FaFacebook className="text-lg" />
+                    </li>
+                    <li className="mr-2">
+                        <FaYoutube className="text-lg" />
+                    </li>
+                    <li className="mr-2">
+                        <FaEnvelope className="text-lg" />
+                    </li>
+                </ul>
                 <div className="self-center lg:hidden">
                     {isOpen ? (
-                        <GrClose className="text-2xl text-gray-500" onClick={() => setIsOpen(!isOpen)} />
+                        <GrClose className="text-2xl text-gray-600" onClick={() => setIsOpen(!isOpen)} />
                     ) : (
                         <FaBars className="text-2xl text-gray-700" onClick={() => setIsOpen(!isOpen)} />
                     )}
                 </div>
             </div>
             <div className="flex lg:w-1/2">
-                <ul className="hidden lg:flex w-full flex-row text-base justify-around items-center text-lg">
-                    <li className="font-medium text-gray-500 lg:hover:text-gray-900">
+                <ul className="hidden lg:flex w-full flex-row text-base justify-around items-center">
+                    <li className="font-bold text-red-800 lg:hover:text-gray-200">
                         <span>Home</span>
                     </li>
-                    <li className="font-medium text-gray-500 hover:text-gray-900">
+                    <li className="font-bold text-red-800 hover:text-gray-200">
                         <span>Our Church</span>
                     </li>
-                    <li className="font-medium text-gray-500 hover:text-gray-900">
+                    <li className="font-bold text-red-800 hover:text-gray-200">
                         <span>Resources</span>
                     </li>
-                    <li className="font-medium text-gray-500 hover:text-gray-900">
+                    <li>
+                        <Image src="/images/LCMS_logo.png" width={75} height={100} />
+                    </li>
+                    <li className="font-bold text-red-800 hover:text-gray-200">
                         <span>Events</span>
                     </li>
-                    <li className="font-medium text-gray-500 hover:text-gray-900">
+                    <li className="font-bold text-red-800 hover:text-gray-200">
                         <span>Gallery</span>
                     </li>
-                    <li className="font-medium text-gray-500 hover:text-gray-900">
+                    <li className="font-bold text-red-800 hover:text-gray-200">
                         <span>Contact</span>
                     </li>
                 </ul>
                 {isOpen && (
                     <ul className="lg:hidden flex flex-col w-full p-2">
                         <div className="border border-gray-200 rounded-md shadow-md">
-                            <li className="py-2 mt-1 border-l-4 border-transparent text-gray-500 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
+                            <li className="py-2 mt-1 border-l-4 border-transparent text-gray-600 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
                                 <span className="pl-4">Home</span>
                             </li>
-                            <li className="py-2 border-l-4 border-transparent text-gray-500 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
+                            <li className="py-2 border-l-4 border-transparent text-gray-600 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
                                 <span className="pl-4">Our Church</span>
                             </li>
-                            <li className="py-2 border-l-4 border-transparent text-gray-500 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
+                            <li className="py-2 border-l-4 border-transparent text-gray-600 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
                                 <span className="pl-4">Resources</span>
                             </li>
-                            <li className="py-2 border-l-4 border-transparent text-gray-500 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
+                            <li className="py-2 border-l-4 border-transparent text-gray-600 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
                                 <span className="pl-4">Events</span>
                             </li>
-                            <li className="py-2 border-l-4 border-transparent text-gray-500 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
+                            <li className="py-2 border-l-4 border-transparent text-gray-600 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
                                 <span className="pl-4">Gallery</span>
                             </li>
-                            <li className="py-2 border-l-4 border-transparent text-gray-500 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
+                            <li className="py-2 border-l-4 border-transparent text-gray-600 font-medium hover:border-blue-600 hover:bg-indigo-100 hover:text-indigo-600 lg:border-0">
                                 <span className="pl-4">Contact</span>
                             </li>
-                            <li className="flex justify-center py-2 font-medium text-gray-500 bg-gray-100 hover:bg-indigo-100 hover:text-indigo-600 rounded-bl-md rounded-br-md">
+                            <li className="flex justify-center py-2 font-medium text-gray-600 bg-gray-100 hover:bg-indigo-100 hover:text-indigo-600 rounded-bl-md rounded-br-md">
                                 <div className="flex">
                                     <span className="mr-2">Support</span>
                                     <FaPrayingHands className="self-center" />
@@ -72,7 +85,7 @@ const Header = (): JSX.Element => {
                 )}
             </div>
             <div className="hidden lg:flex justify-end items-center w-1/4">
-                <Button label="Support" bg="bg-indigo-600" bgHover="hover:bg-blue-900" color="text-white" withIcon icon={<FaPrayingHands />} />
+                <Button label="Support" bg="bg-transparent" bgHover="hover:bg-white" color="text-red-900" withIcon icon={<FaPrayingHands />} extraClassNames="border border-red-900 hover:border-white font-bold" />
             </div>
         </div>
     )
