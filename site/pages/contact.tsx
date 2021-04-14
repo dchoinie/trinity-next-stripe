8a04cb4d1378210051af22c1a04a850dba88e5d4
Dan Choiniere <dchoinie@gmail.com>
2021-04-13 22:13:52 -0500

    Contact form, formspree

--- site/pages/contact.tsx
+++ site/pages/contact.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { NextPage } from 'next'
 import Layout from '../components/layout/Layout'
+import { BiEnvelope, BiMap, BiPhone } from 'react-icons/bi'
+import SectionHeading from '../components/SectionHeading'
 
 const Newsletter = (): JSX.Element => {
     return (
@@ -35,9 +37,47 @@ const Map = (): JSX.Element => {
 
 const ContactForm = (): JSX.Element => {
     return (
-        <form method="POST" action="">
-
-        </form>
+        <div className="max-w-screen-xl mx-auto mb-24">
+            <SectionHeading title="Contact Us" subTitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, delectus?" />
+            <div className="flex gap-12">
+                <form method="POST" action="https://formspree.io/f/xjvjvnpp" className="w-1/2">
+                    <div className="flex gap-6 mb-6">
+                        <label htmlFor="firstName" hidden>First Name</label>
+                        <input type="text" name="firstName" id="firstName" className="w-1/2 p-2 border border-gray-200 rounded shadow-md" placeholder="First Name" required />
+                        <label htmlFor="lastName" hidden>Last Name</label>
+                        <input type="text" name="lastName" id="lastName" className="w-1/2 p-2 border border-gray-200 rounded shadow-md" placeholder="Last Name" required />
+                    </div>
+                    <div className="flex gap-6 mb-6">
+                        <label htmlFor="email" hidden>First Name</label>
+                        <input type="email" name="email" id="email" className="w-1/2 p-2 border border-gray-200 rounded shadow-md" placeholder="Email" required />
+                        <label htmlFor="lastName" hidden>Last Name</label>
+                        <input type="tel" name="phone" id="phone" className="w-1/2 p-2 border border-gray-200 rounded shadow-md" placeholder="Phone" />
+                    </div>
+                    <div className="flex mb-6">
+                        <textarea name="message" id="message" cols={30} rows={10} className="w-full p-2 border border-gray-200 rounded shadow-md" placeholder="Message"></textarea>
+                    </div>
+                    <div className="flex">
+                        <button type="submit" className="flex text-lg font-medium px-5 py-2 rounded shadow-md bg-custom-blue text-gray-200">
+                           Submit
+                        </button>
+                    </div>
+                </form>
+                <div className="flex flex-col w-1/2">
+                    <div className="flex gap-3 text-gray-800 mb-3">
+                        <BiMap className="text-2xl" />
+                        <p className="self-center">415 Lake Street West, Waterville, MN 56096</p>
+                    </div>
+                    <div className="flex gap-3 text-gray-800 mb-3">
+                        <BiPhone className="text-2xl" />
+                        <p className="self-center">507.362.4454</p>
+                    </div>
+                    <div className="flex gap-3 text-gray-800 mb-3">
+                        <BiEnvelope className="text-2xl" />
+                        <p className="self-center">tlchurch@frontiernet.net</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
