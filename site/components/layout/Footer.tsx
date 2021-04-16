3cc0a09ed3ec6e15adbf3bce07ce9104eb076d70
Dan Choiniere <dchoinie@gmail.com>
2021-04-15 21:30:43 -0500

    Footer import order

--- site/components/layout/Footer.tsx
+++ site/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import React from 'react'
+import Link from 'next/link'
 import { FaEnvelope, FaFacebook, FaYoutube } from 'react-icons/fa'
 import Logo from '../../public/images/LCMS_logo.png'
 
