1dd270a93da80e9c6b8afe5f7562a8faab365879
Dan Choiniere <dchoinie@gmail.com>
2021-07-07 20:38:40 -0500

    Finished worship schedule

--- site/pages/worship-schedule.tsx
+++ site/pages/worship-schedule.tsx
@@ -30,6 +30,26 @@ const worshipSchedule = () => {
                     <p>Sundays</p>
                     <p>9:00am</p>
                 </div>
+                <div className="flex flex-col items-center my-6">
+                    <h5>June - August</h5>
+                    <p className="text-gray-500">(Summer Months)</p>
+                </div>
+                <div className="grid grid-cols-3 place-items-center w-1/2 mx-auto">
+                    <p className="underline font-bold mb-2">Event</p>
+                    <p className="underline font-bold mb-2">Day</p>
+                    <p className="underline font-bold mb-2">Time</p>
+                    <p>Divine Service</p>
+                    <p>Sundays</p>
+                    <p>9:00am</p>
+                    <hr className="col-span-3 border-t border-gray-200 w-full my-2" />
+                    <p>Divine Service</p>
+                    <p>Wednesdays</p>
+                    <p>7:00pm</p>
+                    <hr className="col-span-3 border-t border-gray-200 w-full my-2" />
+                    <p>Family Bible Study</p>
+                    <p>Sundays</p>
+                    <p>8:00am</p>
+                </div>
             </div>
         </Layout>
     )
